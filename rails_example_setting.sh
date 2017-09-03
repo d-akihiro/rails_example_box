8098dcb4280faa3c1da1edabd988a93b1d696447
d-akihiro <d_akihiro@outlook.com>
2017-09-03 17:27:30 +0900

    first commit

---
+++ rails_example_setting.sh
@@ -0,0 +1,21 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y git build-essential libssl-dev libreadline-dev libsqlite3-dev nodejs
+
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.profile
+echo 'eval "$(rbenv init -)"' >> ~/.profile
+source ~/.profile
+
+rbenv install 2.4.1
+rbenv rehash
+rbenv global 2.4.1
+
+gem update
+gem install bundler
+
+git clone https://github.com/d-akihiro/rails_example.git ~/work/rails_example
+
+cd ~/work/rails_example
+bundle install
