a48e81e58adad88d8bc96df77e0225633569dc47
d-akihiro <d_akihiro@outlook.com>
2017-10-20 22:52:56 +0900

    インストールするパッケージにtmuxを追加

--- rails_example_setting.sh
+++ rails_example_setting.sh
@@ -1,9 +1,8 @@
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install -y git build-essential libssl-dev libreadline-dev libsqlite3-dev nodejs
+sudo apt-get install -y git build-essential libssl-dev libreadline-dev libsqlite3-dev nodejs tmux
 
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.profile
 echo 'eval "$(rbenv init -)"' >> ~/.profile
 source ~/.profile
