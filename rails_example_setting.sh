6fc390e8ce4d649e329ae7b3f50e3529510ecd21
d-akihiro <d_akihiro@outlook.com>
2017-11-07 21:29:26 +0900

    Rubyのバージョンを変更 2.4.1→2.4.2

--- rails_example_setting.sh
+++ rails_example_setting.sh
@@ -3,18 +3,19 @@ sudo apt-get upgrade
 sudo apt-get install -y git build-essential libssl-dev libreadline-dev libsqlite3-dev nodejs tmux
 
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.profile
 echo 'eval "$(rbenv init -)"' >> ~/.profile
 source ~/.profile
 
-rbenv install 2.4.1
+rbenv install 2.4.2
 rbenv rehash
-rbenv global 2.4.1
+rbenv global 2.4.2
 
 gem update
 gem install bundler
 
-git clone https://github.com/d-akihiro/rails_example.git ~/rails_example
+git clone https://github.com/d-akihiro/rails_example.git ~/work/rails_example_sample
 
-cd ~/rails_example
+cd ~/work/rails_example_sample
 bundle install
